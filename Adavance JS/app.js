7922304de8cbf129916949436235904fac5fca43
vish <meetviveksharma198@gmail.com>
2025-08-15 11:06:54 +0530

    15/8/25

---
+++ Adavance JS/app.js
@@ -0,0 +1,20 @@
+// function one() {
+//   return 1;
+// }
+
+// function two() {
+//   return one() + one();
+// }
+
+// function three() {
+//   let ans = two() + one();
+//   console.log(ans);
+// }
+
+// three();
+
+setTimeout(() => {
+  console.log("Vivek");
+}, 5000);
+
+console.log("Hello!");
