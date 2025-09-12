9759e858615f85d44d9461ad047e3e2572e78980
vish <meetviveksharma198@gmail.com>
2025-09-12 12:17:08 +0530

    ok

--- REST/index.js
+++ REST/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method")); // To use HTTP verbs such as PUT or DELETE in places where the client doesn't support it.
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -11,16 +15,19 @@ app.use(express.static(path.join(__dirname, "public")));
 
 let posts = [
   {
+    id: uuidv4(),
     username: "john_doe",
     content: "Hello, this is my first post!",
   },
 
   {
+    id: uuidv4(),
     username: "jane_smith",
     content: "Excited to join this platform!",
   },
 
   {
+    id: uuidv4(),
     username: "Vivek_Kumar",
     content: "Selelcted for my first job!",
   },
@@ -28,6 +35,44 @@ let posts = [
 
 app.get("/posts", (req, res) => {
   res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("show.ejs", { post });
+});
+
+app.patch("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let newContent = req.body.content;
+  let post = posts.find((p) => p.id === id);
+  post.content = newContent;
+  console.log(post);
+  res.redirect("/posts");
+});
+
+app.get("/posts/:id/edit", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => p.id === id);
+  res.render("edit.ejs", { post });
+});
+
+app.delete("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  posts = posts.filter((p) => p.id !== id);
+  res.redirect("/posts");
 });
 
 app.listen(PORT, () => {
